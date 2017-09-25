61d2c2dcc17ce2f5c267050737a6de4c1d8fe8b8
Vishal Singh Panwar <vishal.singh030788@gmail.com>
2017-09-25 14:18:53 +0530

    Added carthage support

---
+++ Example/InfiniteScrollingFramework/InfiniteScrollingFramework.h
@@ -0,0 +1,19 @@
+//
+//  InfiniteScrollingFramework.h
+//  InfiniteScrollingFramework
+//
+//  Created by Vishal Singh Panwar on 25/09/17.
+//  Copyright © 2017 CocoaPods. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for InfiniteScrollingFramework.
+FOUNDATION_EXPORT double InfiniteScrollingFrameworkVersionNumber;
+
+//! Project version string for InfiniteScrollingFramework.
+FOUNDATION_EXPORT const unsigned char InfiniteScrollingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteScrollingFramework/PublicHeader.h>
+
+
